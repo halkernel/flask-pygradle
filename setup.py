2d3c2044ceaffc5b8c020da9b8190ff94ffcb760
kennet <kennet.emerson@gmail.com>
2020-01-14 01:20:02 -0300

    adding everything

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(name='flaskhelloworld',
+                 version='1.2.3',
+                 package_dir={'':'src'})
+
+#TODO set up the with `open(...` to the current flask directory)
+#TODO set up where the new src is after the previous step
